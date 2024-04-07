276e6886839035ca8bdfab3a1f87ad779a2d4c1e
biminkoju <biminkoju@gmail.com>
2024-04-07 18:53:40 +0545

    america ya hallo hallo hallo hallo hallo hallo hallo hallo hallo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+export default function App() {
+	return (
+		<>
+			<p>america ya</p>
+			<ul>
+				<li>hallo</li>
+				<li>hallo</li>
+				<li>hallo</li>
+				<li>hallo</li>
+				<li>hallo</li>
+				<li>hallo</li>
+				<li>hallo</li>
+				<li>hallo</li>
+			</ul>
+		</>
+	);
+}
